c546647a15073e7ef4efb81fa7398eef6ee2e26d
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2019-03-18 10:47:28 +0200

    Quickstart

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # consensus-viewer
+
 Consensus annotation viewer 
+
+
+## Quickstart
+
+```
+python3 scripts/maketestdb.py test.sqlite examples/standoff/*
+
+./run.sh
+```
+
+(open e.g. http://127.0.0.1:5000/view/17076650 in browser)
---
+++ scripts/maketestdb.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+from sqlitedict import SqliteDict
+
+
+def argparser():
+    from argparse import ArgumentParser
+    ap = ArgumentParser()
+    ap.add_argument('dbname', help='name of database to create')
+    ap.add_argument('file', nargs='+', help='files to insert')
+    return ap
+        
+
+def main(argv):
+    args = argparser().parse_args(argv[1:])
+    with SqliteDict(args.dbname, autocommit=True) as db:
+        for fn in args.file:
+            bn = os.path.basename(fn)
+            db[bn] = open(fn).read()
+
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
